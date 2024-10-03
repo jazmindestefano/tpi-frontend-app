2b474d1755572ff654909043d3f241cf114bca1b
jazmindestefano <jazminisabeldestefano@gmail.com>
2024-10-03 20:59:51 -0300

    feat: add button

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButton from '../Common/Button';
 
 const Header: React.FC = () => {
     return (
@@ -6,13 +7,10 @@ const Header: React.FC = () => {
             <div className="logo">
                 <img src="/path/to/logo.png" alt="Logo" className="h-10" />
             </div>
-            <div className="logout-button">
-                <button 
-                    className="px-4 py-2 bg-blue-500 text-black border-none rounded cursor-pointer"
-                    onClick={() => console.log('Logout clicked')}
-                >
-                    SALIR
-                </button>
+            <div>
+                <CustomButton size={"small"} variant={"primary"}> 
+                    Salir
+                </CustomButton>
             </div>
         </header>
     );
