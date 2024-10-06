5bf1c9eca7dd1bdbeee5e78394a6a5957e7d2737
Lucas David Cardozo <lucas.david.car99@gmail.com>
2024-10-06 17:56:59 -0300

    fix: rename story name to default

--- src/components/Common/Button.stories.ts
+++ src/components/Common/Button.stories.ts
@@ -14,7 +14,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
   },
 };
--- src/components/Layout/PageLayout.stories.tsx
+++ src/components/Layout/PageLayout.stories.tsx
@@ -15,7 +15,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default: Story = {
   parameters: {
     reactRouter: reactRouterParameters({
       routing: reactRouterOutlet(<div></div>)
