e93267a99f55f997ca7ade67025764a833ca8ab1
jazmindestefano <jazminisabeldestefano@gmail.com>
2024-10-12 18:56:43 -0300

    refactor: adjust theme images

--- src/components/common/cards/themeCard/ThemeCard.tsx
+++ src/components/common/cards/themeCard/ThemeCard.tsx
@@ -15,8 +15,8 @@ export const ThemeCard: React.FC<ThemeCardProps> = ({theme, onClick}) => {
   return (
     <BaseCard className={"bg-orange-200 p-4 flex flex-col justify-between items-center"}>
       <BaseContainer className={"gap-6"}>
-        <div onClick={onClick}>
-          <img className="rounded-3xl object-cover h-64 bg-white" src={`/themes/letras/${theme.name}.png`} alt={theme.name}/>
+        <div onClick={onClick} className="w-full">
+          <img className="rounded-3xl bg-white h-80 w-full object-cover" src={`/themes/letras/${theme.name}.png`} alt={theme.name}/>
         </div>
         <Button size={"circle"} shape={"circle"} variant={"secondary"} onClick={() => speakText(theme.name)}>
           <Volume2 color="#ffffff" size={36}/>
