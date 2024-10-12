57757ff66fbbd26a09ba3d08f46bb3ebd8e913d1
jazmindestefano <jazminisabeldestefano@gmail.com>
2024-10-12 18:49:30 -0300

    refactor: home page cards and images

--- src/components/common/cards/HomeCard.tsx
+++ src/components/common/cards/HomeCard.tsx
@@ -8,17 +8,18 @@ import { speakText } from '../../../helpers/speakText';
 
 interface HomeCardProps {
     buttonVariant: 'primary' | 'secondary' | 'tertiary' | 'fourth';
+    backgroundColor: string;
     onClick: () => void;
     game: Game;
 }
 
-const HomeCard: React.FC<HomeCardProps> = ({ buttonVariant, onClick, game }) => {
+const HomeCard: React.FC<HomeCardProps> = ({ buttonVariant, backgroundColor, onClick, game }) => {
     return (
     <BaseCard className={"flex flex-col justify-between items-center w-full gap-4 shadow-none"}>
-        <BaseContainer className={"gap-6 bg-orange-200 rounded-3xl"}>
+        <BaseContainer className={`gap-6 ${backgroundColor} rounded-3xl p-6`}>
             <div onClick={onClick}>
-                <img className="rounded-3xl object-cover h-64" src={`/viborita.svg`} alt={game.name}/>
-                <h1 className="text-3xl font-bold text-center">{game.name}</h1>
+                <img className="rounded-3xl object-cover size-96" src={`/${game.name.toUpperCase()}.png`} alt={game.name}/>
+                <h1 className="text-3xl font-bold text-center">{game.name.toUpperCase()}</h1>
             </div>
         </BaseContainer>
         <Button size="circle" shape="circle" variant={buttonVariant} onClick={() => speakText(game.name)}>
