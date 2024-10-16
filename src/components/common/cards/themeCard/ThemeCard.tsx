7625a1ffebfce30ae41f00f4ec6923a4095bcffb
Jazmín De Stefano <102810217+jdestefano1@users.noreply.github.com>
2024-10-16 15:06:50 -0300

    Feat/snake game (#63)
    * styling snakegame
    
    * feat: do not duplicate vowels
    
    ---------
    
    Co-authored-by: Ain Moises Ponce <ponce.ain@gmail.com>
    Co-authored-by: jazmindestefano <jazminisabeldestefano@gmail.com>

--- src/components/common/cards/themeCard/ThemeCard.tsx
+++ src/components/common/cards/themeCard/ThemeCard.tsx
@@ -16,7 +16,7 @@ export const ThemeCard: React.FC<ThemeCardProps> = ({theme, onClick}) => {
     <BaseCard className={"bg-orange-200 p-4 flex flex-col justify-between items-center"}>
       <BaseContainer className={"gap-6"}>
         <div onClick={onClick} className="w-full">
-          <img className="rounded-3xl bg-white h-80 w-full object-cover" src={`/themes/letras/${theme.name}.png`} alt={theme.name}/>
+          <img className="rounded-3xl bg-white size-80 w-full" src={`/themes/letras/${theme.name}.png`} alt={theme.name}/>
         </div>
         <Button size={"circle"} shape={"circle"} variant={"secondary"} onClick={() => speakText(theme.name)}>
           <VolumeIcon />
