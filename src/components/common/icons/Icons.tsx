f97d58b3e69db9b164d3ddbdcaf9f70821dbfe12
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2024-10-19 16:07:31 -0300

    add left icon

--- src/components/common/icons/Icons.tsx
+++ src/components/common/icons/Icons.tsx
@@ -1,10 +1,12 @@
-import { ArrowBigRight, AudioLines, Mic, Volume2, Smile, Meh, Frown } from "lucide-react";
+import { ArrowBigRight, ArrowBigLeft, AudioLines, Mic, Volume2, Smile, Meh, Frown } from "lucide-react";
 
 export const VolumeIcon = () => <Volume2 color="#ffffff" size={36} />;
 
 export const MicIcon = () => <Mic color="#ffffff" size={36} />;
 
 export const ArrowRightIcon = () => <ArrowBigRight color="#ffffff" size={36} />;
+
+export const ArrowLeftIcon = () => <ArrowBigLeft color="#ffffff" size={36} />;
 
 export const AudioLinesIcon = () => <AudioLines color="#ffffff" size={36} />;
 
