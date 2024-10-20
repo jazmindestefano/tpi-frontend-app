7afcf1a820d39a6085ec4f483feb9f47333ef137
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2024-10-20 19:30:27 -0300

    fix format

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -11,45 +11,33 @@ const Header: React.FC = () => {
 
   return (
     <header className="flex justify-between items-start p-4 bg-gradient-to-b from-orange-100 to-transparent fixed w-full">
-      {!location.pathname.includes("actividad") && (
-      <div className="flex flex-col justify-center items-start gap-4" onClick={() => navigate("/")}>
-        <img src="/clara-logo.svg" alt="Logo" className="h-16 cursor-pointer" />
-      </div>
+      {!location.pathname.includes('actividad') && (
+        <div className="flex flex-col justify-center items-start gap-4" onClick={() => navigate('/')}>
+          <img src="/clara-logo.svg" alt="Logo" className="h-16 cursor-pointer" />
+        </div>
       )}
-      <div className={`flex flex-row gap-4 ${location.pathname.includes("actividad") ? "ml-auto" : ""}`}>
-      <Button
-        size={"square"}
-        variant={"secondary"}
-        onClick={() => navigate("/logros")}
-        >
-        <img
-          src="/pines/medalla.png"
-          alt="Avatar"
-          className="object-cover h-10"
-        />
-      </Button>
-      {location.pathname !== "/perfil" && (
-        <Button
-        size={"square"}
-        variant={"tertiary"}
-        onClick={() => navigate(location.pathname !== "/perfil" ? "/perfil" : "/")}
-        >
-        <img
-          src="/avatar/lion-avatar.png"
-          alt="Avatar"
-          className="object-cover h-10"
-        />
+      <div className={`flex flex-row gap-4 ${location.pathname.includes('actividad') ? 'ml-auto' : ''}`}>
+        <Button size={'square'} variant={'secondary'} onClick={() => navigate('/logros')}>
+          <img src="/pines/medalla.png" alt="Avatar" className="object-cover h-10" />
         </Button>
-      )}
-      {location.pathname === "/" ? (
-        <Button size={"square"} variant={"primary"}>
-        <LogOut className="text-white" />
-        </Button>
-      ) : (
-        <Button size={"square"} variant={"tertiary"} onClick={() => navigate("/")}>
-        <House />
-        </Button>
-      )}
+        {location.pathname !== '/perfil' && (
+          <Button
+            size={'square'}
+            variant={'tertiary'}
+            onClick={() => navigate(location.pathname !== '/perfil' ? '/perfil' : '/')}
+          >
+            <img src="/avatar/lion-avatar.png" alt="Avatar" className="object-cover h-10" />
+          </Button>
+        )}
+        {location.pathname === '/' ? (
+          <Button size={'square'} variant={'primary'}>
+            <LogOut className="text-white" />
+          </Button>
+        ) : (
+          <Button size={'square'} variant={'tertiary'} onClick={() => navigate('/')}>
+            <House />
+          </Button>
+        )}
       </div>
       <div className={`flex flex-row gap-4 ${location.pathname.includes('actividad') ? 'ml-auto' : ''}`}>
         {location.pathname !== '/perfil' && (
