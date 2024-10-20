91108c7c75e04584d029e75dc5bdcfbf29c28971
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2024-10-20 19:32:09 -0300

    format

--- src/__tests__/cards/baseCard.test.tsx
+++ src/__tests__/cards/baseCard.test.tsx
@@ -1,41 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
-import { BaseCard } from '../../components/common/cards/BaseCard';
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import { BaseCard } from '../../components/common/cards/BaseCard'
 
 describe('BaseCard component', () => {
-  
   it('renders children correctly', () => {
     render(
       <BaseCard>
         <p>Test Content</p>
       </BaseCard>
-    );
+    )
 
-    const childElement = screen.getByText(/test content/i);
-    expect(childElement).toBeInTheDocument();
-  });
+    const childElement = screen.getByText(/test content/i)
+    expect(childElement).toBeInTheDocument()
+  })
 
   it('applies the default class name', () => {
     render(
       <BaseCard>
         <p>Test Content</p>
       </BaseCard>
-    );
+    )
 
-    const containerElement = screen.getByText(/test content/i).parentElement;
-    expect(containerElement).toHaveClass('max-w-96 rounded-3xl shadow-lg cursor-pointer');
-  });
+    const containerElement = screen.getByText(/test content/i).parentElement
+    expect(containerElement).toHaveClass('max-w-96 rounded-3xl shadow-lg cursor-pointer')
+  })
 
   it('applies additional className when provided', () => {
     render(
       <BaseCard className="bg-blue-500">
         <p>Test Content</p>
       </BaseCard>
-    );
+    )
 
-    const containerElement = screen.getByText(/test content/i).parentElement;
-    expect(containerElement).toHaveClass('bg-blue-500');
-    expect(containerElement).toHaveClass('max-w-96 rounded-3xl shadow-lg cursor-pointer');
-  });
-
-});
+    const containerElement = screen.getByText(/test content/i).parentElement
+    expect(containerElement).toHaveClass('bg-blue-500')
+    expect(containerElement).toHaveClass('max-w-96 rounded-3xl shadow-lg cursor-pointer')
+  })
+})
--- src/__tests__/modals/baseModal.test.tsx
+++ src/__tests__/modals/baseModal.test.tsx
@@ -1,49 +1,49 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { describe, it, expect, vi } from 'vitest';
-import { BaseModal } from '../../components/common/modals/BaseModal';
+import { render, screen, fireEvent } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import { BaseModal } from '../../components/common/modals/BaseModal'
 
 vi.mock('../../../hooks/useSpeakText', () => ({
-  useSpeakText: () => vi.fn(),
-}));
+  useSpeakText: () => vi.fn()
+}))
 
 describe('BaseModal component', () => {
-  const mockOnClose = vi.fn();
-  
+  const mockOnClose = vi.fn()
+
   it('renders title and children correctly', () => {
     render(
       <BaseModal title="Test Modal" onClose={mockOnClose}>
         <p>Modal content</p>
       </BaseModal>
-    );
+    )
 
-    const titleElement = screen.getByText('Test Modal');
-    const contentElement = screen.getByText('Modal content');
+    const titleElement = screen.getByText('Test Modal')
+    const contentElement = screen.getByText('Modal content')
 
-    expect(titleElement).toBeInTheDocument();
-    expect(contentElement).toBeInTheDocument();
-  });
+    expect(titleElement).toBeInTheDocument()
+    expect(contentElement).toBeInTheDocument()
+  })
 
   it('calls onClose when the close button is clicked', () => {
     render(
       <BaseModal title="Test Modal" onClose={mockOnClose}>
         <p>Modal content</p>
       </BaseModal>
-    );
+    )
 
-    const closeButton = screen.getByText('X');
-    fireEvent.click(closeButton);
+    const closeButton = screen.getByText('X')
+    fireEvent.click(closeButton)
 
-    expect(mockOnClose).toHaveBeenCalled();
-  });
+    expect(mockOnClose).toHaveBeenCalled()
+  })
 
   it('renders the content correctly', () => {
     render(
       <BaseModal title="Test Modal" onClose={mockOnClose}>
         <p>Modal content</p>
       </BaseModal>
-    );
+    )
 
-    const contentElement = screen.getByText('Modal content');
-    expect(contentElement).toBeInTheDocument();
-  });
-});
+    const contentElement = screen.getByText('Modal content')
+    expect(contentElement).toBeInTheDocument()
+  })
+})
