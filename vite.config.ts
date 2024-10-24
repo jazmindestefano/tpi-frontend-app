26b3ced6bfd844af2b1e666fee1ad7ce45d85472
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2024-10-24 13:20:43 -0300

    add vite vercel env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: 'VITE_',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/__tests__/setupTests.ts'
+export default defineConfig(() => {
+  return {
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV
+    },
+    plugins: [react()],
+    envPrefix: 'VITE_',
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/__tests__/setupTests.ts'
+    }
   }
 })
