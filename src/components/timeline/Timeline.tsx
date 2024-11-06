a4b73e3a7bf5e77ca57202ae08d133771ba19c54
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2024-11-06 14:08:18 -0300

    remove min h screen

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -64,7 +64,7 @@ export default function Timeline() {
   }
 
   return (
-    <div className="container p-4 pb-40 min-h-screen h-[calc(100vh-100px)] overflow-y-auto w-full">
+    <div className="container p-4 pb-40 h-[calc(100vh-100px)] overflow-y-auto w-full">
       <BackButton text="Volver al Dashboard" route={`/profesional/paciente/${patientId}`} />
       <h1 className="text-3xl font-bold mb-8 text-gray-800">Línea de Tiempo de Actividades</h1>
       <div className="relative space-y-6 pl-[9%]">
