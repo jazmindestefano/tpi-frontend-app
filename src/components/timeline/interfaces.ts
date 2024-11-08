fb6d37632bd6ab397313243f1694612729faf379
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2024-11-07 23:53:53 -0300

    feat: use timeline tests and fix details with date filter

--- src/components/timeline/interfaces.ts
+++ src/components/timeline/interfaces.ts
@@ -3,3 +3,16 @@ export interface TimelineData {
   gameDescription: string
   playedTimes: number
 }
+
+export interface Game {
+  gameId: number
+  gameName: string
+}
+
+export interface DailyActivity {
+  date: string
+  activities: {
+    name: string
+    count: number
+  }[]
+}
---
+++ src/hooks/timeline/useTimeline.ts
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react'
+import { useGetPatientActivityAnswers, useTimelineData } from '@hooks/queries'
+import { PatientActivityAnswers } from '@interfaces'
+import { Game } from '@components/timeline/interfaces'
+
+const createActivitiesDto = (data: PatientActivityAnswers[]) => {
+  return data.map((activity) => ({
+    gameId: activity.gameid,
+    gameName: activity.gameName
+  }))
+}
+
+const findByGameName = (name: string, activities: Game[]) => {
+  return activities.find((activity) => activity.gameName === name)
+}
+
+const useTimeline = ({ patientId }: { patientId?: string }) => {
+  const [readyToFetch, setReadyToFetch] = useState(false)
+  const { data, isLoading, error } = useTimelineData(readyToFetch ? Number(patientId) : 0)
+  const {
+    data: acts,
+    error: actsErr,
+    isLoading: actsLoading
+  } = useGetPatientActivityAnswers(readyToFetch ? Number(patientId) : 0)
+  const [activities, setActivities] = useState<Game[]>([])
+  const [gameId, setGameId] = useState<number>(0)
+
+  useEffect(() => {
+    if (patientId) {
+      setReadyToFetch(true)
+      if (acts && !actsErr && !actsLoading) {
+        setActivities(createActivitiesDto(acts || []))
+      }
+    }
+  }, [patientId, acts, actsErr, actsLoading])
+
+  useEffect(() => {
+    if (activities) {
+      const game = findByGameName('La Viborita', activities)
+      if (game) {
+        setGameId(game.gameId)
+      }
+    }
+  }, [activities])
+
+  return {
+    gameId,
+    data,
+    isLoading,
+    error,
+    readyToFetch
+  }
+}
+
+export default useTimeline
