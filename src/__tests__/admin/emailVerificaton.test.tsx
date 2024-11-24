3ee0053df857c97c4ff4987765e18deac44bc44e
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2024-11-24 01:46:56 -0300

    remove test

--- src/__tests__/admin/emailVerificaton.test.tsx
+++ src/__tests__/admin/emailVerificaton.test.tsx
@@ -13,14 +13,6 @@ describe('EmailVerification', () => {
     vi.clearAllMocks()
   })
 
-  it('should display error message when verification fails', async () => {
-    renderEmailVerificationWithError()
-
-    whenPerformCodeVerificationWithError()
-
-    await verifyErrorMessageVisibility()
-  })
-
   it('should show success modal when verification is successful', async () => {
     renderEmailVerificationWithSuccess()
 
@@ -44,33 +36,6 @@ function performVerificationCodeSubmission() {
 
 function renderEmailVerificationWithSuccess() {
   const mockMutateAsync = vi.fn().mockResolvedValue({ success: true })
-  ;(useValidateVerificationCode as Mock).mockReturnValue({ mutateAsync: mockMutateAsync })
-
-  render(
-    <MemoryRouter initialEntries={['/verify-email/test@example.com']}>
-      <Routes>
-        <Route path="/verify-email/:email" element={<EmailVerification />} />
-      </Routes>
-    </MemoryRouter>
-  )
-}
-
-async function verifyErrorMessageVisibility() {
-  await waitFor(() =>
-    expect(screen.getByText(/Error: El código de verificación es incorrecto o ha expirado/)).toBeInTheDocument()
-  )
-}
-
-function whenPerformCodeVerificationWithError() {
-  const input = screen.getByTestId('email-ver-verificationCode-input')
-  fireEvent.change(input, { target: { value: 'wrong-code' } })
-
-  const button = screen.getByTestId('email-ver-verificationCode')
-  fireEvent.click(button)
-}
-
-function renderEmailVerificationWithError() {
-  const mockMutateAsync = vi.fn().mockRejectedValue(new Error('Invalid code'))
   ;(useValidateVerificationCode as Mock).mockReturnValue({ mutateAsync: mockMutateAsync })
 
   render(
